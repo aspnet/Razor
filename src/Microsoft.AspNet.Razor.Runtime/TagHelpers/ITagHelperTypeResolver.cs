d140ac5fcf58a7b4338737c5cc978e7de236740e
N. Taylor Mullen <nimullen@microsoft.com>
2014-09-18 10:43:09 -0700

    Add TagHelperDescriptorResolver implementation.
    - Added a basic TagHelperDescriptor creation mechanism to the TagHelperDescriptorResolver.
    - Added an ITagHelperTypeResolver to be responsible for determining valid TagHelper types.
    
    #99

---
+++ src/Microsoft.AspNet.Razor.Runtime/TagHelpers/ITagHelperTypeResolver.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+
+namespace Microsoft.AspNet.Razor.Runtime.TagHelpers
+{
+    /// <summary>
+    /// Defines a class that is used to resolve tag helper <see cref="Type"/>s.
+    /// </summary>
+    public interface ITagHelperTypeResolver
+    {
+        /// <summary>
+        /// Resolves tag helper <see cref="Type"/>s.
+        /// </summary>
+        /// <param name="lookupText">
+        /// A <see cref="string"/> location on where to find tag helper <see cref="Type"/>s.
+        /// </param>
+        /// <returns>An <see cref="IEnumerable{Type}"/> of <see cref="Type"/>s that represent tag helpers.</returns>
+        IEnumerable<Type> Resolve(string lookupText);
+    }
+}
