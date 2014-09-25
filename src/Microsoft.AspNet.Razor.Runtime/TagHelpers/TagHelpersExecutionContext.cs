1ba05a508f6be2f00b104514775056b613d76de5
NTaylorMullen <nimullen@microsoft.com>
2014-09-25 01:04:55 -0700

    Add TagHelperRunner for TagHelper runtime.
    - This involved adding the following core classes: TagHelper, TagHelperExecutionContext, TagHelperOutput, TagHelperContext.  All of which aid in running TagHelpers.
    
    #154

---
+++ src/Microsoft.AspNet.Razor.Runtime/TagHelpers/TagHelpersExecutionContext.cs
@@ -0,0 +1,81 @@
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+
+namespace Microsoft.AspNet.Razor.Runtime.TagHelpers
+{
+    /// <summary>
+    /// Defines a class that is used to store information about a <see cref="TagHelper"/>s execution lifetime.
+    /// </summary>
+    public class TagHelpersExecutionContext
+    {
+        /// <summary>
+        /// Instantiates a new <see cref="TagHelpersExecutionContext"/>.
+        /// </summary>
+        /// <param name="tagName">The HTML tag name associated with the current <see cref="TagHelpersExecutionContext"/>.</param>
+        public TagHelpersExecutionContext(string tagName)
+        {
+            AllAttributes = new Dictionary<string, object>(StringComparer.Ordinal);
+            HTMLAttributes = new Dictionary<string, string>(StringComparer.Ordinal);
+            TagHelpers = new List<TagHelper>();
+            TagName = tagName;
+        }
+
+        /// <summary>
+        /// HTML attributes for the current execution context.
+        /// </summary>
+        public Dictionary<string, string> HTMLAttributes { get; private set; }
+
+        /// <summary>
+        /// <see cref="TagHelper"/> bound attributes and HTML attributes.
+        /// </summary>
+        public Dictionary<string, object> AllAttributes { get; private set; }
+
+        /// <summary>
+        /// <see cref="TagHelper"/>s that should be run for the current <see cref="TagHelpersExecutionContext"/>.
+        /// </summary>
+        public List<TagHelper> TagHelpers { get; private set; }
+
+        /// <summary>
+        /// The HTML tag name for the current <see cref="TagHelpersExecutionContext"/>.
+        /// </summary>
+        public string TagName { get; private set; }
+
+        /// <summary>
+        /// The <see cref="TagHelperOutput"/> of the current <see cref="TagHelpersExecutionContext"/>.
+        /// </summary>
+        public TagHelperOutput Output { get; set; }
+
+        /// <summary>
+        /// Tracks the given <paramref name="tagHelper"/>.
+        /// </summary>
+        /// <param name="tagHelper">The tag helper to track.</param>
+        public void Add(TagHelper tagHelper)
+        {
+            TagHelpers.Add(tagHelper);
+        }
+
+        /// <summary>
+        /// Tracks the HTML attribute.
+        /// </summary>
+        /// <param name="name">The HTML attribute name.</param>
+        /// <param name="value">The HTML attribute value.</param>
+        public void AddHtmlAttribute(string name, string value)
+        {
+            HTMLAttributes.Add(name, value);
+            AllAttributes.Add(name, value);
+        }
+
+        /// <summary>
+        /// Tracks the HTML attribute so it can be used by a <see cref="TagHelperContext.AllAttributes"/>.
+        /// </summary>
+        /// <param name="name">The HTML attribute name.</param>
+        /// <param name="value">The attribute value.</param>
+        public void AddTagHelperAttribute(string name, object value)
+        {
+            AllAttributes.Add(name, value);
+        }
+    }
+}
