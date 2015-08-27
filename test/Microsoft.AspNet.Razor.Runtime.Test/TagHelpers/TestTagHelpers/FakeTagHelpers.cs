fd9276d4e604837bd8d62c3c48c7b93847eb9751
Pranav K <prkrishn@hotmail.com>
2015-08-26 19:21:38 -0700

    Produce ITypeInfo \ IPropertyInfo using CodeAnalysis symbol tree

---
+++ test/Microsoft.AspNet.Razor.Runtime.Test/TagHelpers/TestTagHelpers/FakeTagHelpers.cs
@@ -0,0 +1,34 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Razor.Runtime.TagHelpers;
+
+namespace Microsoft.AspNet.Razor.Fake
+{
+    public interface ITagHelper
+    {
+
+    }
+
+    public class DoesNotImplementRealITagHelper : Microsoft.AspNet.Razor.Fake.ITagHelper
+    {
+    }
+
+    public class ImplementsRealITagHelper : Microsoft.AspNet.Razor.Runtime.TagHelpers.ITagHelper
+    {
+        public int Order
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
