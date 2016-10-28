bce5d0719b4d332e93af0ef3199cd36e2e4e9ecf
Jass Bagga <jbagga93@live.ca>
2016-10-28 11:12:47 -0700

    Moved location of and changed CaseSensitiveTagHelperAttributeComparer to a more generic one

--- test/Microsoft.AspNetCore.Razor.Runtime.Test/TagHelpers/ReadOnlyTagHelperAttributeListTest.cs
+++ test/Microsoft.AspNetCore.Razor.Runtime.Test/TagHelpers/ReadOnlyTagHelperAttributeListTest.cs
@@ -4,6 +4,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using Microsoft.AspNetCore.Razor.Test.Internal;
 using Xunit;
 
 namespace Microsoft.AspNetCore.Razor.TagHelpers
