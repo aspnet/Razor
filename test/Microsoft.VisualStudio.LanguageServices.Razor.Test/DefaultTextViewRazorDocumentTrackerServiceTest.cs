62c0044e0b58b5e8369822e9718351cb844edbdc
Ryan Nowak <nowakra@gmail.com>
2017-04-24 14:01:18 -0700

    Adds a tracking system for Razor documents
    This is a MEF service that can actively or passively track open ITextViews
    and give us information about the Razor initialization state and eventing
    when it changes.
    
    The purpose of this is to act as a bridge between the VS mef world and the
    roslyn world.
    
    For now this doesn't do any passive tracking of Razor documents, it's only
    on demand. That means it will only be initialized and used right now when
    you are using the Razor developer tools. This is just to reduce our risk,
    it's not ideal to ship code in VS that's doing something without anyone
    looking at the result.

---
+++ test/Microsoft.VisualStudio.LanguageServices.Razor.Test/DefaultTextViewRazorDocumentTrackerServiceTest.cs
@@ -0,0 +1,82 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Collections.ObjectModel;
+using Microsoft.CodeAnalysis.Razor;
+using Microsoft.VisualStudio.Text;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Utilities;
+using Moq;
+using Xunit;
+
+namespace Microsoft.VisualStudio.LanguageServices.Razor
+{
+    public class DefaultTextViewRazorDocumentTrackerServiceTest
+    {
+        [Fact]
+        public void CreateTracker_CreatesNewTracker_WhenDocumentNotTracked()
+        {
+            // Arrange
+            var service = CreateTrackerService();
+
+            var textView = CreateTextView();
+
+            // Act
+            var tracker = service.CreateTracker(textView);
+
+            // Assert
+            Assert.NotNull(tracker);
+            Assert.Same(tracker, textView.Properties.GetProperty(typeof(RazorDocumentTracker)));
+        }
+
+        [Fact]
+        public void CreateTracker_ReturnsExistingTracker_WhenDocumentIsAlreadyTracked()
+        {
+            // Arrange
+            var service = CreateTrackerService();
+
+            var textView = CreateTextView();
+
+            var expected = new DefaultTextViewRazorDocumentTracker(service, textView);
+            textView.Properties.AddProperty(typeof(RazorDocumentTracker), expected);
+
+            // Act
+            var tracker = service.CreateTracker(textView);
+
+            // Assert
+            Assert.NotNull(tracker);
+            Assert.Same(tracker, expected);
+        }
+
+        [Fact]
+        public void SubjectBuffersConnected_CreatesAndReturnsTracker()
+        {
+            // Arrange
+            var service = CreateTrackerService();
+
+            var textView = CreateTextView();
+
+            // Act
+            service.SubjectBuffersConnected(textView, ConnectionReason.BufferGraphChange, new Collection<ITextBuffer>());
+
+            // Assert
+            Assert.NotNull(textView.Properties.GetProperty(typeof(RazorDocumentTracker)));
+        }
+
+        private IWpfTextView CreateTextView()
+        {
+            var properties = new PropertyCollection();
+            var textView = Mock.Of<IWpfTextView>(t => t.Properties == properties);
+            return textView;
+        }
+
+        private DefaultTextViewRazorDocumentTrackerService CreateTrackerService()
+        {
+            var services = Mock.Of<IServiceProvider>();
+            var documentFactory = Mock.Of<ITextDocumentFactoryService>();
+            var workspace = Mock.Of<VisualStudioWorkspace>();
+            return new DefaultTextViewRazorDocumentTrackerService(services, documentFactory, workspace);
+        }
+    }
+}
