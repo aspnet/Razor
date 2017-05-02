2ebf10b507234e66e75f71d961476baeee3f7a00
Yves57 <ymarx@wanadoo.fr>
2017-05-02 21:24:37 +0200

    fix: rebase to make one unit test ok.

--- test/Microsoft.AspNetCore.Razor.Language.Test/TestFiles/IntegrationTests/CodeGenerationIntegrationTest/IncompleteDirectives_Runtime.codegen.cs
+++ test/Microsoft.AspNetCore.Razor.Language.Test/TestFiles/IntegrationTests/CodeGenerationIntegrationTest/IncompleteDirectives_Runtime.codegen.cs
@@ -9,21 +9,33 @@ namespace Microsoft.AspNetCore.Razor.Language.IntegrationTests.TestFiles
         #pragma warning disable 1998
         public async System.Threading.Tasks.Task ExecuteAsync()
         {
-            WriteLiteral("\r\n\r\n");
-            WriteLiteral("\r\n");
-            WriteLiteral("\r\n");
-            WriteLiteral("\r\n");
-            WriteLiteral("\r\n");
-            WriteLiteral("\r\n\r\n");
+            WriteLiteral(@"
+
+");
+            WriteLiteral(@"
+");
+            WriteLiteral(@"
+");
+            WriteLiteral(@"
+");
+            WriteLiteral(@"
+");
+            WriteLiteral(@"
+
+");
             DefineSection("", async () => {
             });
-            WriteLiteral("\r\n");
+            WriteLiteral(@"
+");
             DefineSection("", async () => {
             });
-            WriteLiteral("\r\n\r\n");
+            WriteLiteral(@"
+
+");
             DefineSection("", async () => {
             });
-            WriteLiteral("{\r\n");
+            WriteLiteral(@"{
+");
         }
         #pragma warning restore 1998
     }
