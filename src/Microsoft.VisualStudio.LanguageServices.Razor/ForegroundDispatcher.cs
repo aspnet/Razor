14a3b10065284d763f2da7135c16356ade92e932
Ryan Nowak <nowakra@gmail.com>
2017-08-24 07:58:18 -0700

    Add foreground dispatcher

---
+++ src/Microsoft.VisualStudio.LanguageServices.Razor/ForegroundDispatcher.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Runtime.CompilerServices;
+
+namespace Microsoft.VisualStudio.LanguageServices.Razor
+{
+    internal abstract class ForegroundDispatcher
+    {
+        public abstract bool IsForegroundThread { get; }
+
+        public virtual void AssertForegroundThread([CallerMemberName] string caller = null)
+        {
+
+        }
+
+        public abstract void AssertBackgroundThread([CallerMemberName] string caller = null)
+        {
+
+        }
+    }
+}
