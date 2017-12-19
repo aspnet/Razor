32b4fe910faeec2b1826936eebdd28770fefef3c
Pranav K <prkrishn@hotmail.com>
2017-12-19 14:09:37 -0800

    Make RazorCodeGen track file renames \ deletes
    Fixes #1810

--- test/Microsoft.AspNetCore.Razor.Design.Test/IntegrationTests/MSBuildIntegrationTestBase.cs
+++ test/Microsoft.AspNetCore.Razor.Design.Test/IntegrationTests/MSBuildIntegrationTestBase.cs
@@ -2,9 +2,11 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Threading;
 using System.Threading.Tasks;
+using Moq;
 
 namespace Microsoft.AspNetCore.Razor.Design.IntegrationTests
 {
@@ -24,9 +26,9 @@ namespace Microsoft.AspNetCore.Razor.Design.IntegrationTests
 #error Configuration not supported
 #endif
 
-        protected string IntermediateOutputPath => Path.Combine("obj", Configuration, TargetFramework);
+        protected string IntermediateOutputPath => Path.Combine(Project.DirectoryPath, "obj", Configuration, TargetFramework);
 
-        protected string OutputPath => Path.Combine("bin", Configuration, TargetFramework);
+        protected string OutputPath => Path.Combine(Project.DirectoryPath, "bin", Configuration, TargetFramework);
 
         // Used by the test framework to set the project that we're working with
         internal static ProjectDirectory Project
@@ -45,6 +47,30 @@ namespace Microsoft.AspNetCore.Razor.Design.IntegrationTests
             var restoreArgument = suppressRestore ? "" : "/restore";
 
             return MSBuildProcessManager.RunProcessAsync(Project, $"{restoreArgument} /t:{target} /p:Configuration={Configuration} {args}", timeout);
+        }
+
+        /// <summary>
+        /// Locks all files, discovered at the time of method invocation, under the
+        /// specified <paramref name="directory" /> from reads or writes.
+        /// </summary>
+        public IDisposable LockDirectory(string directory)
+        {
+            var disposables = new List<IDisposable>();
+            foreach (var file in Directory.EnumerateFiles(directory, "*", SearchOption.AllDirectories))
+            {
+                disposables.Add(LockFile(file));
+            }
+
+            var disposable = new Mock<IDisposable>();
+            disposable.Setup(d => d.Dispose())
+                .Callback(() => disposables.ForEach(d => d.Dispose()));
+
+            return disposable.Object;
+        }
+
+        public IDisposable LockFile(string path)
+        {
+            return File.Open(path, FileMode.Open, FileAccess.Read, FileShare.None);
         }
     }
 }
--- test/Microsoft.AspNetCore.Razor.Design.Test/IntegrationTests/RazorGenerateIntegrationTest.cs
+++ test/Microsoft.AspNetCore.Razor.Design.Test/IntegrationTests/RazorGenerateIntegrationTest.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using System;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
@@ -10,11 +11,13 @@ namespace Microsoft.AspNetCore.Razor.Design.IntegrationTests
 {
     public class RazorGenerateIntegrationTest : MSBuildIntegrationTestBase
     {
+        private const string RazorGenerateTarget = "RazorGenerate";
+
         [Fact]
         [InitializeTestProject("SimpleMvc")]
         public async Task RazorGenerate_Success_GeneratesFilesOnDisk()
         {
-            var result = await DotnetMSBuild("RazorGenerate");
+            var result = await DotnetMSBuild(RazorGenerateTarget);
 
             Assert.BuildPassed(result);
 
@@ -31,6 +34,125 @@ namespace Microsoft.AspNetCore.Razor.Design.IntegrationTests
             Assert.FileExists(result, RazorIntermediateOutputPath, "Views", "Shared", "_ValidationScriptsPartial.cs");
             Assert.FileExists(result, RazorIntermediateOutputPath, "Views", "Shared", "Error.cs");
             Assert.FileCountEquals(result, 8, RazorIntermediateOutputPath, "*.cs");
+        }
+
+        [Fact]
+        [InitializeTestProject("SimpleMvc")]
+        public async Task RazorGenerate_BuildsIncrementally()
+        {
+            // Act - 1
+            var result = await DotnetMSBuild(RazorGenerateTarget);
+            var generatedFile = Path.Combine(RazorIntermediateOutputPath, "Views", "Home", "About.cs");
+
+            // Assert - 1
+            Assert.BuildPassed(result);
+            Assert.FileExists(result, generatedFile);
+            var thumbPrint = FileThumbPrint.Create(generatedFile);
+
+            // Act - 2
+            using (var razorGenDirectoryLock = LockDirectory(RazorIntermediateOutputPath))
+            {
+                result = await DotnetMSBuild(RazorGenerateTarget);
+            }
+                
+            // Assert - 2
+            Assert.BuildPassed(result);
+            Assert.FileExists(result, generatedFile);
+            var currentThumbPrint = FileThumbPrint.Create(generatedFile);
+            Assert.Equal(thumbPrint, currentThumbPrint);
+        }
+
+        [Fact]
+        [InitializeTestProject("SimpleMvc")]
+        public async Task RazorGenerate_Rebuilds_IfSourcesAreUpdated()
+        {
+            // Act - 1
+            var result = await DotnetMSBuild(RazorGenerateTarget);
+            var file = Path.Combine(Project.DirectoryPath, "Views", "Home", "Contact.cshtml");
+            var generatedFile = Path.Combine(RazorIntermediateOutputPath, "Views", "Home", "Contact.cs");
+            var updateContent = Environment.NewLine + "<h1>Hello world</h1>";
+
+            // Assert - 1
+            Assert.BuildPassed(result);
+            var fileThumbPrint = FileThumbPrint.Create(generatedFile);
+
+            // Act - 2
+            // Update the source content and build. We should expect the outputs to be regenerated.
+            File.AppendAllText(file, updateContent);
+            result = await DotnetMSBuild(RazorGenerateTarget);
+
+            // Assert - 2
+            Assert.BuildPassed(result);
+            var newThumbPrint = FileThumbPrint.Create(generatedFile);
+            Assert.NotEqual(fileThumbPrint, newThumbPrint);
+        }
+
+        [Fact]
+        [InitializeTestProject("SimpleMvc")]
+        public async Task RazorGenerate_Rebuilds_IfOutputFilesAreMissing()
+        {
+            // Act - 1
+            var result = await DotnetMSBuild(RazorGenerateTarget);
+            var file = Path.Combine(RazorIntermediateOutputPath, "Views", "Home", "About.cs");
+
+            // Assert - 1
+            Assert.BuildPassed(result);
+            Assert.FileExists(result, file);
+
+            // Act - 2
+            File.Delete(file);
+            result = await DotnetMSBuild(RazorGenerateTarget);
+
+            // Assert - 2
+            Assert.BuildPassed(result);
+            Assert.FileExists(result, Path.Combine(RazorIntermediateOutputPath, "Views", "Home", "About.cs"));
+        }
+
+        [Fact]
+        [InitializeTestProject("SimpleMvc")]
+        public async Task RazorGenerate_Rebuilds_IfInputFilesAreRenamed()
+        {
+            // Act - 1
+            var result = await DotnetMSBuild(RazorGenerateTarget);
+            var file = Path.Combine(Project.DirectoryPath, "Views", "Home", "Index.cshtml");
+            var renamed = Path.Combine(Project.DirectoryPath, "Views", "Home", "NewIndex.cshtml");
+            var generated = Path.Combine(RazorIntermediateOutputPath, "Views", "Home", "Index.cs");
+
+            // Assert - 1
+            Assert.BuildPassed(result);
+            Assert.FileExists(result, file);
+            Assert.FileExists(result, generated);
+
+            // Act - 2
+            File.Move(file, renamed);
+            result = await DotnetMSBuild(RazorGenerateTarget);
+
+            // Assert - 2
+            Assert.BuildPassed(result);
+            Assert.FileExists(result, RazorIntermediateOutputPath, "Views", "Home", "NewIndex.cs");
+            Assert.FileDoesNotExist(result, generated);
+        }
+
+        [Fact]
+        [InitializeTestProject("SimpleMvc")]
+        public async Task RazorGenerate_Rebuilds_IfInputFilesAreDeleted()
+        {
+            // Act - 1
+            var result = await DotnetMSBuild(RazorGenerateTarget);
+            var file = Path.Combine(Project.DirectoryPath, "Views", "Home", "Index.cshtml");
+            var generatedFile = Path.Combine(RazorIntermediateOutputPath, "Views", "Home", "Index.cs");
+
+            // Assert - 1
+            Assert.BuildPassed(result);
+            Assert.FileExists(result, generatedFile);
+
+            // Act - 2
+            File.Delete(file);
+            result = await DotnetMSBuild(RazorGenerateTarget);
+
+            // Assert - 2
+            Assert.BuildPassed(result);
+            Assert.FileDoesNotExist(result, generatedFile);
         }
     }
 }
