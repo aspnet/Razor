dee4201d306a8f6c25086dc2abce1b3a6c32ceb1
Ryan Nowak <nowakra@gmail.com>
2017-12-20 18:29:28 -0800

    Add metadata to Razor
    This PR introduces standard metadata to Razor. This change begins to
    formalize the contract between generated code produced by Razor and
    runtimes that want to load and interact with this code.
    
    This is a step on making MVC a 'plugin' to Razor rather than the only
    possible implementation. Since we're doing MSBuild work - this is the
    right time to designate the current interaction between Razor and MVC as
    'legacy' and move forward.
    
    Additionally, we need the source thumbprinting to make re-compilation of
    Razor a thing.
    
    -----
    
    Also I noticed that our source document doesn't expose the hash
    algorithm by name. We really should have this, so I added it and
    hardened the code that deals with checksums in c# generation.

---
+++ src/Microsoft.AspNetCore.Razor.Runtime/Hosting/DefaultRazorCompiledItem.cs
@@ -0,0 +1,38 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+
+namespace Microsoft.AspNetCore.Razor.Hosting
+{
+    internal class DefaultRazorCompiledItem : RazorCompiledItem
+    {
+        public DefaultRazorCompiledItem(Type type, string kind, string identifier)
+        {
+            if (type == null)
+            {
+                throw new ArgumentNullException(nameof(type));
+            }
+
+            if (kind == null)
+            {
+                throw new ArgumentNullException(nameof(kind));
+            }
+
+            if (identifier == null)
+            {
+                throw new ArgumentNullException(nameof(identifier));
+            }
+
+            Type = type;
+            Kind = kind;
+            Identifier = identifier;
+        }
+
+        public override string Identifier { get; }
+
+        public override string Kind { get; }
+
+        public override Type Type { get; }
+    }
+}
