a6f6755ab1c29a1dcc2a33eadead099eb461302c
Ajay Bhargav Baaskaran <ajbaaska@microsoft.com>
2018-09-26 12:41:36 -0700

    New baselines for HtmlTagTest and HtmlErrorTest

--- test/Microsoft.AspNetCore.Razor.Language.Test/Legacy/HtmlErrorTest.cs
+++ test/Microsoft.AspNetCore.Razor.Language.Test/Legacy/HtmlErrorTest.cs
@@ -7,6 +7,11 @@ namespace Microsoft.AspNetCore.Razor.Language.Legacy
 {
     public class HtmlErrorTest : CsHtmlMarkupParserTestBase
     {
+        public HtmlErrorTest()
+        {
+            UseNewSyntaxTree = true;
+        }
+
         [Fact]
         public void AllowsInvalidTagNamesAsLongAsParserCanIdentifyEndTag()
         {
